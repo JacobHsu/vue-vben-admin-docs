e53066b7f7505246c990b5506717c7e92322de50
JacobHsu <jacob.hsu.tw@gmail.com>
2021-07-16 07:22:31 +0000

    Deploying to gh-pages from  @ 37f2ed1531842a7a4c4c0e212b03c69fb20b4159 🚀

---
+++ assets/components_transition.md.80c22929.lean.js
@@ -0,0 +1,1 @@
+import{o as n,c as s,a}from"./app.384067ab.js";const t='{"title":"Transition","description":"","frontmatter":{},"headers":[{"level":2,"title":"Usage","slug":"usage"}],"relativePath":"components/transition.md","lastUpdated":1626420122773}',p={},o=a('',4);p.render=function(a,t,p,c,e,u){return n(),s("div",null,[o])};export default p;export{t as __pageData};
