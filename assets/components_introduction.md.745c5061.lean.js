20d4b2f2314afabb5cdd1a2c3396884fdadd749c
JacobHsu <jacob.hsu.tw@gmail.com>
2021-07-21 06:47:47 +0000

    Deploying to gh-pages from  @ 73814a4857e278af4c0e7d7fc496d3561a486743 🚀

---
+++ assets/components_introduction.md.745c5061.lean.js
@@ -0,0 +1,1 @@
+import{o as n,c as a,a as s}from"./app.09f17e8a.js";const t='{"title":"前言","description":"","frontmatter":{},"headers":[{"level":2,"title":"Usage","slug":"usage"}],"relativePath":"components/introduction.md","lastUpdated":1626850040571}',p={},o=s('',5);p.render=function(s,t,p,e,c,l){return n(),a("div",null,[o])};export default p;export{t as __pageData};
