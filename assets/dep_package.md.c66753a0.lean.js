2898bb7734c962a03a78a740ebd3fe84eeb199b8
JacobHsu <jacob.hsu.tw@gmail.com>
2021-09-22 07:37:31 +0000

    Deploying to gh-pages from  @ 5f268114ca815505c8cf5153b49143af694c7674 🚀

---
+++ assets/dep_package.md.c66753a0.lean.js
@@ -0,0 +1,1 @@
+import{o as s,c as a,a as n}from"./app.7716e933.js";const t='{"title":"package.json","description":"","frontmatter":{},"headers":[{"level":2,"title":"http-server","slug":"http-server"}],"relativePath":"dep/package.md","lastUpdated":1632296217011}',e={},p=n('',8);e.render=function(n,t,e,o,r,c){return s(),a("div",null,[p])};export default e;export{t as __pageData};
---
+++ assets/other_qa.md.84e2e913.lean.js
@@ -0,0 +1,1 @@
+import{o as n,c as e,a as s}from"./app.7716e933.js";const a='{"title":"QA","description":"","frontmatter":{},"relativePath":"other/qa.md","lastUpdated":1632296217015}',o={},t=s('',13);o.render=function(s,a,o,p,c,r){return n(),e("div",null,[t])};export default o;export{a as __pageData};
